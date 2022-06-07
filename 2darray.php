a908b0bb92298b05735d75ae109b802309b869b1
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-06-08 03:12:28 +0530

    starting code in php by using xammp localhost here i have created 2d array in php and constants in php

---
+++ 2darray.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+$cars = array
+  (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+  );
+ 
+echo "Accessing the elements of the array.<br><br>" ;
+echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br><br>";
+echo "Looping through the array.<br><br>";
+ 
+for ($row = 0; $row < 4; $row++) {
+  echo "<p><b>Row number $row</b></p>";
+  echo "<ul>";
+  for ($col = 0; $col < 3; $col++) {
+	echo "<li>".$cars[$row][$col]."</li>";
+  }
+  echo "</ul>";
+}
+?>
+ 
+</body>
+</html>
---
+++ constant.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <?php
+        define("student",["   kirti  ","    bca   ","  ipu  "]);
+        echo student[0];
+        echo student[1];
+        echo student[2];
+        ?>
+    </body>
+</html>
