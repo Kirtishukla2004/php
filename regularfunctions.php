84be6b26fb4c48190f580bf8b0f2aa98d17679ea
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-06-09 20:38:32 +0530

    Add files via upload

--- regularfunctions.php
+++ regularfunctions.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-$str = "the man on street are all cool";
+$str = "the man on street are all cool ";
 $pattern = "/man/i";
 echo preg_replace($pattern, "men", $str);
 echo preg_match_all($pattern, $str);
