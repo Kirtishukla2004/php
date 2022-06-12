e972018bddaa6059295043d5254574a4b64764d5
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-06-12 21:52:07 +0530

    printing name by using php and using if and if else statements

---
+++ name.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>practical 1</title>
+    </head>
+    <body>
+        <?php
+        if(10>2){
+            echo"kirti<br>";
+            if(10>15){
+                echo"shukla <br>";
+            }
+            else{
+                echo"Kirti shukla";
+            }
+        }
+        ?>
+    </body>
+</html>
+
