31641c2e0d5a265749c76a7d570e7e2e64a016f2
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-06-18 12:41:09 +0530

    mathematical functions in php code

---
+++ maths.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo(pi());
+echo (min(0, 150, 30, 20, -8, -200)."<br>");  // returns -200
+echo(max(0, 150, 30, 20, -8, -200)."<br>");  // returns 150
+echo(abs(-6.7)."<br>");  // returns 6.7
+echo(sqrt(64)."<br>");  // returns 8
+echo(round(0.60)."<br>");  // returns 1
+echo(round(0.49)."<br>");  // returns 0
+echo(rand()."<br>");
+?>
+
+</body>
+</html>
