56c3055a028886505506122361556f14342a71c9
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-06-21 19:59:30 +0530

    PHP Casting Strings and Floats to Integers in php code

---
+++ ftoi.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Cast float to int 
+$x = 23465.768;
+$int_cast = (int)$x;
+echo $int_cast;
+  
+echo "<br>";
+
+// Cast string to int
+$x = "23465.768";
+$int_cast = (int)$x;
+echo $int_cast;
+?>  
+
+</body>
+</html>
