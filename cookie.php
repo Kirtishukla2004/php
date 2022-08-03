9c154990e19b96ed61c6324162338e76dfe217f0
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-08-03 09:53:38 +0530

    Add files via upload

---
+++ cookie.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html
+<body>
+<?php
+$cookie_name = "user";
+$cookie_value = "Alex Porter";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+
+
+
+
+     echo "Cookie '" . $cookie_name . "' is set!<br>";
+     echo "Value is: " . $_COOKIE[$cookie_name];
+
+?>
+
+<p><strong>Note:</strong> You might have to reload the page to see the new value of the cookie.</p>
+
+</body>
+</html>
