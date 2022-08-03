02eeb9dfa5e24361f5eefeb7fc059f50a8690e42
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-08-03 09:54:01 +0530

    Add files via upload

---
+++ nestedif.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html><?php  
+               $age = 23;  
+    $nationality = "Indian";  
+    //applying conditions on nationality and age  
+    if ($nationality == "Indian")  
+    {  
+        if ($age >= 18) {  
+            echo "Eligible to give vote";  
+        }  
+        else {    
+            echo "Not eligible to give vote";  
+        }  
+    }  
+?>  
