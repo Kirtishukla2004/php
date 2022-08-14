721025056e5163879ad73acad2f51cba43ff7860
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-08-14 22:06:44 +0530

    Add files via upload

---
+++ strings.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strlen("Hello world!");
+echo str_word_count("Hello world!");
+echo strrev("Hello world!");
+echo strpos("Hello world!", "world");
+echo str_replace("world", "Dolly", "Hello world!");
+?> 
+ 
+</body>
+</html>
